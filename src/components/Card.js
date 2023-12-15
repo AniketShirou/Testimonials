2ea5b86a567720838e53afc4778b16cda5adb2ba
AniketShirou <aniketranawork@gmail.com>
2023-12-15 17:32:54 +0530

    my first commit

---
+++ src/components/Card.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {FaQuoteLeft , FaQuoteRight} from "react-icons/fa";
+
+
+const Card = (props) =>{
+    let review = props.review;
+  return(
+    <div className="flex flex-col md:relative">
+        <div className="absolute top-[-7rem] z-[10] mx-auto">
+            <img  className="aspect-square rounded-full w-[140px] h-[140px] z-30" src={review.image}></img>
+            <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-20] left-[5px]"></div>
+        </div>
+
+        <div className="text-center mt-7">
+            <p className="font-bold text-2xl capitalize tracking-wider">{review.name}</p>
+        </div>
+
+        <div className="text-center mt-1">
+            <p className="text-violet-400 uppercase text-sm">{review.job}</p>
+        </div>
+
+        <div className="text-violet-500 mx-auto mt-5">
+            <FaQuoteLeft/>
+        </div>
+
+        <div className="text-center mt-4 text-sl text-slate-600">
+            <p>{review.text}</p>
+        </div>
+
+        <div className="text-violet-500 mx-auto mt-5">
+            <FaQuoteRight/>
+        </div>
+
+       
+
+    </div>
+  )
+}
+
+export default Card;
